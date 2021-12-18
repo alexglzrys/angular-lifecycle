fb010db3dd57928954bec36667d855046d7be9b9
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-18 15:06:23 -0600

    explicación sobre los ciclos de vida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { Pagina1Component } from './pages/pagina1/pagina1.component';
     Pagina1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
@@ -8,45 +8,67 @@ import { Component, OnInit, SimpleChanges } from '@angular/core';
 })
 export class Pagina1Component implements OnInit {
 
+  nombre: string = 'Alejandro';
+
   constructor() {
-    console.log('constructor - especial para inicializar las propiedades de este componente, así como inyectar servicios')
+    console.warn('1. constructor')
+    console.log('Crea la nueva instancia de este componente, especial para inyectar servicios')
+    console.log('---')
+  }
+
+  /**
+   * Los ciclos de vida son métodos que deben ser implementados para lanzar cierta lógica durante la creación, cambio o destrucción de un componente
+   * No se requiere implementar todos ellos dentro de un componente
+   * Va a depender de la necesidad el usarlos o no.
+   */
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.error('** ngOnChanges **')
+    console.log('Se ejecuta cuando cambia un valor de un input control dentro de un componente, o cuando cambia el valor de una propiedad vinculada. Siempre recibe un mapa de datos con el valor actual y anterior de la propiedad que ha cambiado su valor en cuestión')
+    console.log(SimpleChange)
+    console.log('---')
   }
 
   ngOnInit(): void {
-    console.log('ngOnInit - especial para hacer peticiones http')
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngOnChanges - Detectar cambios en alguna propiedad del componente')
-
+    console.warn('2. ngOnInit')
+    console.log('El componente ha sido inicializado, especial para hacer peticiones http o inicializar la data del componente')
+    console.log('---')
   }
 
   ngDoCheck(): void {
-    console.log('ngDoCheck - detecta camnios en las propiedades de entrada')
-
-  }
-
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit - Llamado cuando la vista se ha inicializado')
-
-  }
-
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked - llamado despues de que se haya checado que cargaron bien las vistas o componentes hijos de este componente')
+    console.warn('3. ngDoCheck')
+    console.log('Se ejecuta cada vez que se verifican las propiedades de entrada en el componente')
+    console.log('---')
   }
 
   ngAfterContentInit(): void {
-    console.log('ngAfterContentInit - llamado despues de ngonInit al proyectar contenido externo en el componente')
-
+    console.warn('4. ngAfterContentInit')
+    console.log('Se ejecuta cuando se muestra el contenido dentro de las vistas del componente "componentes hijo"')
+    console.log('---')
   }
 
   ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked - responde despues del contenido proyectado ')
+    console.warn('5. ngAfterContentChecked')
+    console.log('Se ejecuta cuando el contenido de los componentes hijos ha sido verificado por el mecanismo de detección de cambios de Angular')
+    console.log('---')
+  }
+
+  ngAfterViewInit(): void {
+    console.warn('6. ngAfterViewInit')
+    console.log('Se ejecuta cuando la vista completa del componente (hijos y contenido directo) se ha inicializado por completo')
+    console.log('---')
+  }
+
+  ngAfterViewChecked(): void {
+    console.warn('7. ngAfterViewChecked')
+    console.log('Se ejecuta cada vez que la vista del componente verifique cambios, es útil cuando el componente espera algún valor que proviene de sus componentes secundario (custom event)')
+    console.log('---')
   }
 
   ngOnDestroy(): void {
-    console.log('ngOnDestroy - tareas de limpieza')
-
+    console.warn('8. ngOnDestroy')
+    console.log('Se ejecuta cuando el componente se destruye, útil para dar de baja a suscripción de onbservables y desconectar controladores de eventos para evitar fugas de memoria')
+    console.log('---')
   }
 
 }
